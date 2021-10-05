23cdf377e930b36f6f9fc9c737c36d3b65154760
Aaron Russell Jr <adrusselljr@Aarons-MBP.lan>
2021-10-05 14:23:52 -0400

    modified js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const prompt = require("prompt-sync")({ sigint:true })
 
-console.log("Welcome to the To-Do List Manager Application! ")
+console.log("Welcome to the To-Do List Manager Application!")
 
 console.log("==============================================")
 
@@ -16,33 +16,91 @@ while(true) {
         console.log(`You have ${list.length} to-do item(s).`)
         
         for(let i = 0; i < list.length; i++) {
-            let output = isComplete[i] === true ? "[Complete]" : "[Incomplete]"
+            const output = isComplete[i] === true ? "[Complete]" : "[Incomplete]"
             console.log(`${i + 1}. ${output} ${list[i]}`)
+        }
+    }
+
+    let areAnyComplete = false
+    for(let i = 0; i < isComplete.length; i++) {
+        const complete1 = isComplete[i]
+        if(complete1 === true) {
+            areAnyComplete = true
         }
     }
     
     console.log("~ Select an action ~")
+
     console.log("[1] Create a to-do item")
-    console.log("[2] Complete a to-do item")
+
+    if(list.length > 0) {
+        if(areAnyComplete === false) {
+            console.log("[2] Complete a to-do item")
+        }
+        else {
+            console.log("[2] Complete a to-do item")
+            console.log("[3] Incomplete a to-do item")
+        }
+        console.log("[4] Delete a to-do item")
+    }
     const todo = Number(prompt(""))
     
     if(todo === 1) {
-        console.log("~ Creating a new to-do item ~")
-        let addTodo = prompt("What is this to-do item called? ")
-        list.push(addTodo)
-        isComplete.push(false)
+        function todo1() {
+            console.log("~ Creating a new to-do item ~")
+
+            let addTodo = prompt("What is this to-do item called? ")
+
+            list.push(addTodo)
+            isComplete.push(false)
+        }
+        todo1()
     }
         
     if(todo === 2) {
-        console.log("~ Completing a to-do item ~")
-
-        let completeTodo = Number(prompt("Which to-do item would you like to complete? "))
-
-        for(let i = 0; i < isComplete.length; i++) {
-            if(completeTodo === i + 1) {
-                isComplete[i] = true
+        function todo2() {
+            console.log("~ Completing a to-do item ~")
+    
+            let completeTodo = Number(prompt("Which to-do item would you like to complete? "))
+    
+            for(let i = 0; i < isComplete.length; i++) {
+                if(completeTodo === i + 1) {
+                    isComplete[i] = true
+                }
             }
         }
+        todo2()
+    }
+
+    if(todo === 3) {
+        function todo3() {
+            console.log("~ Incompleting a to-do item ~")
+    
+            let incompleteTodo = Number(prompt("Which to-do item would you like to incomplete? "))
+    
+            for(let i = 0; i < isComplete.length; i++) {
+                if(incompleteTodo === i + 1) {
+                    isComplete[i] = false
+                }
+            }
+        }
+        todo3()
+    }
+
+    if(todo === 4) {
+        function deleteitem() {
+            console.log("~ Deleting a to-do item ~")
+    
+            let deleteitem = Number(prompt("Which to-do item would you like to delete? "))
+    
+            for(let i = 0; i < list.length; i++) {
+                if(deleteitem === i + 1) {
+                    list.splice(list[i], 1)
+                    isComplete.splice(isComplete[i], 1)
+                }
+            }
+        }
+        deleteitem()
     }
 
 }
